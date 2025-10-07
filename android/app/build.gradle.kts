249910162227f1ba55734504a9e0c2e45014c2a4
Suhas <pentakotasuhas@gmail.com>
2025-10-08 00:41:13 +0530

    Fix formatting in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,4 +39,4 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
     implementation("androidx.media:media:1.7.0")
-}.
+}
